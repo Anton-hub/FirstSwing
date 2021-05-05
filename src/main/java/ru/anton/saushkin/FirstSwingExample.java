6ffa55a3f626fc56bfded723b0ed0bac59526b53
Anton <saushkin_anton@mail.ru>
2021-05-05 23:09:57 +0300

    Добавил поле для ввода

--- src/main/java/ru/anton/saushkin/FirstSwingExample.java
+++ src/main/java/ru/anton/saushkin/FirstSwingExample.java
@@ -4,16 +4,23 @@ import javax.swing.*;
 
 public class FirstSwingExample {
     public static void main(String[] args) {
-        JFrame f=new JFrame();//creating instance of JFrame
+        JFrame frame = new JFrame();//creating instance of JFrame
 
-        JButton b=new JButton("click");//creating instance of JButton
-        b.setBounds(130,100,100, 40);//x axis, y axis, width, height
+        frame.setSize(500,300);//400 width and 500 height
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setVisible(true);//making the frame visible
+        frame.setSize(300, 300);
 
-        f.add(b);//adding button in JFrame
+        JButton button = new JButton("click");//creating instance of JButton
+        button.setBounds(5,5,100, 25);//x axis, y axis, width, height
 
-        f.setSize(400,500);//400 width and 500 height
-        f.setLayout(null);//using no layout managers
-        f.setVisible(true);//making the frame visible
+        JTextField textField = new JTextField(10);
+        textField.setBounds(110,5, 200, 25);
+
+        frame.add(button);//adding button in JFrame
+        frame.add(textField);
+
     }
 }
 
